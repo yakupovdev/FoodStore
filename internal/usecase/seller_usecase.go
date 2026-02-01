f81faba20d2d7f0b9296bed22da5772a8285e997
yakupovdev <yakupovdev@gmail.com>
2026-02-01 22:42:09 +0500

    feat(seller): add GET seller`s profile & offers, POST creation of offers

---
+++ internal/usecase/seller_usecase.go
@@ -0,0 +1,61 @@
+package usecase
+
+import (
+	"github.com/yakupovdev/FoodStore/internal/model"
+	"github.com/yakupovdev/FoodStore/internal/repository"
+)
+
+type SellerUsecase struct {
+	repo *repository.SellerRepository
+}
+
+func NewSellerUsecase(repo *repository.SellerRepository) *SellerUsecase {
+	return &SellerUsecase{
+		repo: repo,
+	}
+}
+
+func (uc *SellerUsecase) GetProfileByID(userID int64) (model.SellerProfileResponse, error) {
+	seller, err := uc.GetProfileByID(userID)
+	if err != nil {
+		return model.SellerProfileResponse{}, err
+	}
+
+	return model.SellerProfileResponse{
+		Name:    seller.Name,
+		Email:   seller.Email,
+		Type:    seller.Type,
+		Balance: seller.Balance,
+		Rating:  seller.Rating,
+	}, nil
+
+}
+
+func (uc *SellerUsecase) GetSellerOffersByID(userID int64) (model.SellerOffersResponse, error) {
+	offers, err := uc.repo.GetSellerOffers(userID)
+	if err != nil {
+		return model.SellerOffersResponse{}, err
+	}
+
+	return model.SellerOffersResponse{
+		Offers: offers,
+	}, nil
+}
+
+func (uc *SellerUsecase) CreateSellerOffer(req model.CreateSellerOfferRequest, userID int64) error {
+	err := uc.repo.CreateSellerOffer(model.CreateOfferParams{
+		SellerID:        userID,
+		ProductName:     req.ProductName,
+		Description:     req.Description,
+		Image:           req.Image,
+		Price:           req.Price,
+		Quantity:        req.Quantity,
+		CategoryName:    req.CategoryName,
+		SubCategoryName: req.SubCategoryName,
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
